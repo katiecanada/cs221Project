1f2565e7fb23786e3cc4270f0c39fab96dfd97ff
Chelsea Lewis <cwlewis28@gmail.com>
2014-11-08 13:23:09 -0800

    added helper functions

--- baseline.py
+++ baseline.py
@@ -1,3 +1,18 @@
+''' git commit -a -m "comment" '''
+
+def HingeLossGradient(w, y, phi):
+    '''
+    Given weights (w), y, and phi, the function computes and returns the
+    corresponding Hinge Loss Gradient. 
+    '''
+    check = dotProduct(w, phi) * y
+    if check > 1: return {}
+    else: 
+        phi_new = {}
+        increment(phi_new, -1 * y, phi)
+        return phi_new
+
+
 def learnPredictor(trainExamples, testExamples, featureExtractor):
     '''
     Given |trainExamples| and |testExamples| (each one is a list of (x,y)
@@ -29,3 +44,26 @@ def learnPredictor(trainExamples, testExamples, featureExtractor):
 
     # END_YOUR_CODE
     return weights
+
+    def dotProduct(d1, d2):
+    """
+    @param dict d1: a feature vector represented by a mapping from a feature (string) to a weight (float).
+    @param dict d2: same as d1
+    @return float: the dot product between d1 and d2
+    """
+    if len(d1) < len(d2):
+        return dotProduct(d2, d1)
+    else:
+        return sum(d1.get(f, 0) * v for f, v in d2.items())
+
+def increment(d1, scale, d2):
+    """
+    Implements d1 += scale * d2 for sparse vectors.
+    @param dict d1: the feature vector which is mutated.
+    @param float scale
+    @param dict d2: a feature vector.
+    """
+    for f, v in d2.items():
+        d1[f] = d1.get(f, 0) + v * scale
+
+    
